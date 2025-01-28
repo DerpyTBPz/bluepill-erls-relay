b91e5fa2a0d0233ef5165f1f1295474b5cbbb4da
DerpyTBPz <dima.plyasov13@gmail.com>
2025-01-28 11:59:37 +0200

    test-0.2

---
+++ Core/Src/main.c
@@ -0,0 +1,505 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdbool.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define CRSF_MAX_SIZE 64
+#define CIRCULAR_BUFF_MAX_SIZE 128
+
+#define CRSF_SYNC 0xc8
+#define CRSF_LQ 0x14
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t circBuff1[CIRCULAR_BUFF_MAX_SIZE];
+uint8_t circBuff2[CIRCULAR_BUFF_MAX_SIZE];
+
+uint8_t RxBuff1[CIRCULAR_BUFF_MAX_SIZE];
+uint8_t RxBuff2[CIRCULAR_BUFF_MAX_SIZE];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_TIM2_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_DMA(&huart2, circBuff1, CIRCULAR_BUFF_MAX_SIZE);
+  HAL_UART_Receive_DMA(&huart3, circBuff2, CIRCULAR_BUFF_MAX_SIZE);
+  HAL_TIM_Base_Start_IT(&htim2);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+  while (1)
+  {
+	  HAL_UART_Receive_DMA(&huart2, circBuff1, CIRCULAR_BUFF_MAX_SIZE);
+	  HAL_UART_Receive_DMA(&huart3, circBuff2, CIRCULAR_BUFF_MAX_SIZE);
+//	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+//	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
+//	  HAL_Delay(1000);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+int count1 = 0;
+int count2 = 0;
+
+int leftover1 = 0;
+int leftover2 = 0;
+
+int lq1 = 100;
+int lq2 = 50;
+
+int STATUS1 = 0;
+int STATUS2 = 0;
+
+void ScanDataLQ1()
+{
+	bool flag1 = false;
+	while (!flag1)
+	{
+		if ((STATUS1 == 0) || (STATUS1 == 1))
+		{
+			for (int i = count1; i < CIRCULAR_BUFF_MAX_SIZE; i++)
+			{
+				flag1 = false;
+				if (RxBuff1[i] == CRSF_SYNC)
+				{
+					count1 = i;
+					STATUS1 = ProcessDataLQ1();
+					if (STATUS1 == 1)
+					{
+						count1 = 0;
+						leftover1 = 0;
+						flag1 = true;
+					}
+					else if (STATUS1 == 2)
+					{
+						count1 = 0;
+						flag1 = true;
+						break;
+					}
+					else
+					{
+						count1 = 0;
+					}
+				}
+				flag1 = true;
+			}
+		}
+		else if (STATUS1 == 2)
+		{
+			lq1 = RxBuff1[leftover1 - 1];
+			flag1 = false;
+			count1 = leftover1;
+			STATUS1 = 0;
+		}
+	}
+}
+
+void ScanDataLQ2()
+{
+	bool flag2 = false;
+	while (!flag2)
+	{
+		if ((STATUS2 == 0) || (STATUS2 == 1))
+		{
+			for (int i = count2; i < CIRCULAR_BUFF_MAX_SIZE; i++)
+			{
+				flag2 = false;
+				if (RxBuff2[i] == CRSF_SYNC)
+				{
+					count2 = i;
+					STATUS2 = ProcessDataLQ2();
+					if (STATUS2 == 1)
+					{
+						count2 = 0;
+						leftover2 = 0;
+						flag2 = true;
+					}
+					else if (STATUS2 == 2)
+					{
+						count2 = 0;
+						flag2 = true;
+						break;
+					}
+					else
+					{
+						count2 = 0;
+					}
+				}
+				flag2 = true;
+			}
+		}
+		else if (STATUS2 == 2)
+		{
+			lq2 = RxBuff2[leftover2 - 1];
+			flag2 = false;
+			count2 = leftover2;
+			STATUS2 = 0;
+		}
+	}
+}
+
+int ProcessDataLQ1()
+{
+	int avLen = CIRCULAR_BUFF_MAX_SIZE - count1;
+	if (avLen >= 6)
+	{
+		if (RxBuff1[count1 + 2] == CRSF_LQ)
+		{
+			lq1 = RxBuff1[count1 + 5];
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+
+	}
+	else if (avLen < 5)
+	{
+		leftover1 = 6 - avLen;
+		if (leftover1 >= 3)
+		{
+			if (RxBuff1[count1 + 3] == CRSF_LQ)
+			{
+				return 2;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
+
+int ProcessDataLQ2()
+{
+	int avLen = CIRCULAR_BUFF_MAX_SIZE - count2;
+	if (avLen >= 6)
+	{
+		if (RxBuff2[count2 + 2] == CRSF_LQ)
+		{
+			lq2 = RxBuff2[count2 + 5];
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+
+	}
+	else if (avLen < 5)
+	{
+		leftover2 = 6 - avLen;
+		if (leftover2 >= 3)
+		{
+			if (RxBuff2[count2 + 3] == CRSF_LQ)
+			{
+				return 2;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
+
+void ScanDataLQ(int* count, int* leftover, int* status, uint8_t* rxBuff, int* lq, int (*processDataLQ)(int*, int*, uint8_t*, int*)) {
+    bool flag = false;
+    int check = 0;
+    while (!flag)
+    {
+        if ((*status == 0) || (*status == 1))
+        {
+            for (int i = *count; i < CIRCULAR_BUFF_MAX_SIZE; i++)
+            {
+                flag = false;
+                if (rxBuff[i] == CRSF_SYNC)
+                {
+                    *count = i;
+                    check = 0;
+                    *status = processDataLQ(count, leftover, rxBuff, lq);
+                    if (*status == 1)
+                    {
+                        *count = 0;
+                        *leftover = 0;
+                        flag = true;
+                    }
+                    else if (*status == 2)
+                    {
+                        *count = 0;
+                        flag = true;
+                        break;
+                    }
+                    else
+                    {
+                        *count = 0;
+                    }
+                }
+                else
+                {
+                	check++;
+                }
+                flag = true;
+            }
+            if (check >= (CIRCULAR_BUFF_MAX_SIZE - 3))
+            {
+            	*lq = 0;
+            	check = 0;
+            }
+        }
+        else if (*status == 2)
+        {
+            *lq = rxBuff[*leftover - 1];
+            flag = false;
+            *count = *leftover;
+            *status = 0;
+        }
+    }
+}
+
+int ProcessDataLQ(int* count, int* leftover, uint8_t* rxBuff, int* lq)
+{
+    int avLen = CIRCULAR_BUFF_MAX_SIZE - *count;
+    if (avLen >= 6)
+    {
+        if (rxBuff[*count + 2] == CRSF_LQ)
+        {
+            *lq = rxBuff[*count + 5];
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else if (avLen < 5)
+    {
+        *leftover = 6 - avLen;
+        if (*leftover >= 3)
+        {
+            if (rxBuff[*count + 3] == CRSF_LQ)
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
+
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (huart == &huart2)
+	{
+		HAL_UART_Receive_DMA(&huart3, circBuff2, CIRCULAR_BUFF_MAX_SIZE);
+		memcpy(RxBuff1, circBuff1, CIRCULAR_BUFF_MAX_SIZE);
+//		ScanDataLQ1();
+		ScanDataLQ(&count1, &leftover1, &STATUS1, RxBuff1, &lq1, ProcessDataLQ);
+
+
+
+	}
+	else if (huart == &huart3)
+	{
+		HAL_UART_Receive_DMA(&huart2, circBuff1, CIRCULAR_BUFF_MAX_SIZE);
+		memcpy(RxBuff2, circBuff2, CIRCULAR_BUFF_MAX_SIZE);
+//		ScanDataLQ2();
+		ScanDataLQ(&count2, &leftover2, &STATUS2, RxBuff2, &lq2, ProcessDataLQ);
+	}
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+//
+//	if (htim == &htim2)
+//	{
+//		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+
+//		if ((lq1 < 50) || (lq2 < 50))
+//		{
+	if ((abs(lq1 - lq2) > 5) && ((lq1 < 60) || (lq2 < 60)))
+	{
+		if (lq1 > lq2)
+		{
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+		}
+		else if (lq1 < lq2)
+		{
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+		}
+	}
+//		}
+//	}
+
+
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
